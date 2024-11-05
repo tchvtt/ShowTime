25ac7f65c9eed3104bf0d0a86cd0b9c4150a1582
ThomasC <tchvtt@gmail.com>
2024-11-05 17:42:41 +0100

    ShowTimeApp Creation

--- src/main/java/com/ShowTime/ShowTimeApplication.java
+++ src/main/java/com/ShowTime/ShowTimeApplication.java
@@ -2,12 +2,20 @@ package com.ShowTime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
 
 @SpringBootApplication
+@RestController
 public class ShowTimeApplication {
+    public static void main(String[] args) {
+      SpringApplication.run(ShowTimeApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShowTimeApplication.class, args);
-	}
-
+    @GetMapping("/ShowTime")
+    public String showtime() {
+      return String.format("It's ShowTime !");
+    }
 }
