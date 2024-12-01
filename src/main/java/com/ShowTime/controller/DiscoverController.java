54739cd546b9f3c0afb99e5c36f1dc17e7f29ccf
ThomasC <tchvtt@gmail.com>
2024-12-01 09:24:54 +0100

    css-update

--- src/main/java/com/ShowTime/controller/DiscoverController.java
+++ src/main/java/com/ShowTime/controller/DiscoverController.java
@@ -1,14 +1,12 @@
 package com.ShowTime.controller;
 
 import java.util.List;
-import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ShowTime.model.Movie;
 import com.ShowTime.model.TVShow;
